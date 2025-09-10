959ad36c8625366bb1c03627c7172e66e431ddb2
LautiiGomez <lautigomezsalvi@gmail.com>
2025-09-10 12:35:43 -0300

    Mejorando los modelos pero le falta, inicialize el alembic pero falta configurarlo/investigarlo.

--- backend/app/models/ORM_Producto.py
+++ backend/app/models/ORM_Producto.py
@@ -7,4 +7,5 @@ class Producto(Base): # Renombrado a Producto para mayor claridad
     id_producto = Column(Integer, primary_key=True, index=True)
     nombre_producto = Column(String, index=True)
     descripcion_producto = Column(String, index=True) 
-    cantidad_producto = Column(Integer, index=True) 
+    cantidad_producto = Column(Integer, index=True)
+    precio_producto = Column(Float, index=True)
