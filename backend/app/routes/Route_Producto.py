8cd6f1dc8dc05ecea9d455fa71f784c4d337c5d2
LautiiGomez <lautigomezsalvi@gmail.com>
2025-10-17 12:38:36 -0300

    Crear ruta para añadir producto

--- backend/app/routes/Route_Producto.py
+++ backend/app/routes/Route_Producto.py
@@ -6,7 +6,8 @@ from typing import List
 
 from app.database.db import get_db
 from app.models.ORM_Producto import Producto as ORMProducto
-from app.schemas.Producto import Producto
+from app.schemas.Producto import Producto, ProductoCreate
+from backend.auth import get_current_user
 
 router = APIRouter(
     prefix="/productos",
@@ -21,18 +22,31 @@ def leer_productos(db: Session = Depends(get_db)): # Conectarse a la db
 
 # Endpoint para obtener un producto por ID (sin autenticación)
 @router.get("/{producto_id}", response_model=Producto)
-def leer_producto_por_id(producto_id: int, db: Session = Depends(get_db)):
-    producto = db.get(ORMProducto, producto_id)
-    if producto is None:    # Si el producto no se encuentra, devolver un producto no encontrado y el famoso error 404
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Producto no encontrado :("
-        )
-    return producto
+def leer_producto_por_id(
+    producto_id: int, 
+    db: Session = Depends(get_db)):
+        producto = db.get(ORMProducto, producto_id)
+        if producto is None:    # Si el producto no se encuentra, devolver un producto no encontrado y el famoso error 404
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Producto no encontrado :("
+            )
+        return producto
 
-@router.post("/")
-def crear_producto():
-    return {"mensaje": "Servidor funcionando y ruta de productos crear OK"}
+# Endpoint para crear un nuevo producto (con autenticación)
+@router.post("/", response_model=Producto, status_code=status.HTTP_201_CREATED)
+def crear_producto(
+    producto: ProductoCreate,                     # Recibe los datos del producto en formato JSON según el esquema ProductoCreate
+    db: Session = Depends(get_db),                # Conectarse a la base de datos
+    current_user: dict = Depends(get_current_user) # Verifica que el usuario esté autenticado
+):
+    db_producto = ORMProducto(**producto.dict())  # Crea un objeto ORMProducto con los datos recibidos
+    db.add(db_producto)                           # Agrega el producto a la sesión de la base de datos
+    db.commit()                                   # Confirma los cambios en la base de datos
+    db.refresh(db_producto)                       # Actualiza el objeto con valores generados automáticamente (como id)
+    return db_producto                            # Devuelve el producto creado al cliente
+
+
 @router.put("/{producto_id}")
 def actualizar_producto(producto_id):
     return {"mensaje": "Servidor funcionando y ruta de productos actualizar OK"}
