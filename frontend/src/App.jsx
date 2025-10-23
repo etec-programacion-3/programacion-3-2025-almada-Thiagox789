b1a9b6a7e31b5a045bb33afa057453901588553e
Thiagox789 <t.almada@alumno.etec.um.edu.ar>
2025-10-23 15:49:59 -0300

    Terminada la Issue 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import Inicio from './components/Inicio';
+import Productos from './components/Productos';
+import Login from './components/Login';
+import Carrito from './components/Carrito';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <ul>
+          <li><Link to="/">Inico</Link></li>
+          <li><Link to="/Productos">Productos</Link></li>
+          <li><Link to="/Login">Login</Link></li>
+          <li><Link to="/Carrito">Carrito</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/Productos" element={<Productos />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Carrito" element={<Carrito />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/components/Carrito.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Carrito() {
+  return (
+    <div>
+      <h1>Pagina De carrito</h1>
+    </div>
+  );
+}
+
+export default Carrito;
