63a507b4f8c16871a6aaa6e959b8831c11a05c7a
Thiagox789 <t.almada@alumno.etec.um.edu.ar>
2025-11-06 16:45:22 -0300

    Mejoritas

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,12 +4,16 @@ import '../App.css'; // Assuming App.css contains some basic styling
 
 const ProductCard = ({ product }) => {
   return (
-    <Link to={`/products/${product.id_producto}`} className="product-card-link"> {/* Wrap with Link */}
+    <Link to={`/products/${product.id_producto}`} className="product-card-link">
       <div className="product-card">
-        <h3>{product.nombre_producto}</h3>
-        <p>{product.descripcion_producto}</p>
-        <p>Price: ${product.precio_producto}</p>
-        <img src={product.image_url}/>
+        <div className="product-image-container">
+          <img src={product.image_url} alt={product.nombre_producto} />
+        </div>
+        <div className="product-info">
+          <h3>{product.nombre_producto}</h3>
+          <p>{product.descripcion_producto}</p>
+          <p className="product-price">${product.precio_producto}</p>
+        </div>
       </div>
     </Link>
   );
